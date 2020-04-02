197b58a19fecba819e3d0f2ea3a92b6dc9de440b
Achraf BELLA <52492864+loverman1@users.noreply.github.com>
2020-04-02 21:20:54 +0200

    Update and rename Serveur to Serveur.js

---
+++ Serveur.js
@@ -0,0 +1,9 @@
+
+var http = require('http'),
+ms = require('mediaserver');
+
+http.createServer(function (req, res) {
+
+    ms.pipe(req, res, "C:\\Users\\mesda\\Documents\\MUSIC\\Maître Gims - Sapés comme jamais (Clip officiel) f(MP3_128K).mp3");
+
+}).listen(8080, '127.0.0.1');
